58480a8d45b864f6ca3853d818cd1d23ee5604c1
ParthR23 <prambhorkar@gmail.com>
2025-07-25 00:06:44 +0530

    release: main.py changes

--- src/main/transformations/jobs/main.py
+++ src/main/transformations/jobs/main.py
@@ -1,11 +1,13 @@
 import os
 
 from resources.dev import config
+from src.main.read.aws_read import S3Reader
 from src.main.utility.encrypt_decrypt import *
 from src.main.utility.my_sql_session import get_mysql_connection
 from src.main.utility.s3_client_object import  *
 from src.main.utility.logging_config import *
 from src.test.generate_csv_data import start_date
+from src.test.scratch_pad import folder_path, s3_absolute_file_path
 
 ################# Get S3 Client ###################
 aws_access_key = config.aws_access_key
@@ -51,3 +53,17 @@ if csv_files:
 
 else:
     logger.info("Last run was successful!!!")
+
+try:
+    s3_reader = S3Reader()
+    #Bucket name should come from table
+    folder_path = config.s3_source_directory
+    s3_absolute_file_path = s3_reader.list_files(s3_client,config.bucket_name,folder_path=folder_path)
+    logger.info("Absolute path on S3 bucket for csv file %s ", s3_absolute_file_path)
+    if not s3_absolute_file_path:
+        logger.info(f"No files available at {folder_path}")
+        raise Exception("No data available to process")
+
+except Exception as e:
+    logger.error("Exited with error:- %s",e)
+    raise e
